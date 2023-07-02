802d57883245f41791585cfd614767141f2577d8
dorbh123 <dorbhdorbh@gmail.com>
2023-07-02 22:38:27 +0300

    FireBaseLogin

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    //Screen object variables
+    public GameObject loginUI;
+    
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    //Functions to change the login screen UI
+    public void LoginScreen() //Back button
+    {
+        loginUI.SetActive(true);
+       
+    }
+ 
+}
